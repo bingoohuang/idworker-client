5ffe93188a15dc5657cd18d6ae8a765e3e6e38b8
bingoohuang <bingoo.huang@gmail.com>
2014-08-21 22:25:13 +0800

    fix test case

--- src/test/java/org/n3r/idworker/SidTest.java
+++ src/test/java/org/n3r/idworker/SidTest.java
@@ -11,7 +11,7 @@ public class SidTest {
         System.out.println(Sid.next());
         System.out.println(Sid.nextShort());
         for (int i = 0; i < 10000; ++i) {
-            assertThat(Sid.next().length(), is(20));
+            assertThat(Sid.next().length(), is(21));
             assertThat(Sid.nextShort().length(), is(16));
         }
     }
