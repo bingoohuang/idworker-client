388acf0b4907015d2b39dd5dab51bb772d03a98c
bingoohuang <bingoo.huang@gmail.com>
2014-08-29 14:36:58 +0800

    refactorings

--- src/main/java/org/n3r/idworker/utils/HttpReq.java
+++ src/main/java/org/n3r/idworker/utils/HttpReq.java
@@ -7,9 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.*;
 
 public class HttpReq {
     private final String baseUrl;
